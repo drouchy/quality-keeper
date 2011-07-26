e7fc7a3307436d40fe4d304c30d70f46dbf0c098
David Rouchy <drouchy@gmail.com>
2011-07-26 19:50:40 +0200

    starting to implement the projects index pages

--- spec/views/projects/index.html.erb_spec.rb
+++ spec/views/projects/index.html.erb_spec.rb
@@ -2,39 +2,64 @@ require 'spec_helper'
 
 describe "projects/index.html.erb" do
   before(:each) do
-    assign(:projects, [
-      stub_model(Project,
-        :name => "Name",
-        :description => "Description",
-        :logo => "Logo",
-        :git_url => "Git Url",
-        :git_login => "Git Login",
-        :git_password => "Git Password"
-      ),
-      stub_model(Project,
-        :name => "Name",
-        :description => "Description",
-        :logo => "Logo",
-        :git_url => "Git Url",
-        :git_login => "Git Login",
-        :git_password => "Git Password"
-      )
-    ])
+    @projects = [
+      FactoryGirl.build(:project, :id => 1),
+      FactoryGirl.build(:project, :id => 2)
+    ]
+    
+    assign(:projects, @projects)
   end
-
-  it "renders a list of projects" do
-    render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Description".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Logo".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Git Url".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Git Login".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Git Password".to_s, :count => 2
+  
+  describe it "render a list a project" do
+    
+    it 'renders a li per project' do
+      render
+      
+      assert_select "ul#projects>li.project", :count => 2
+    end
+    
+    it 'renders the name of the project' do
+      render
+      
+      @projects.each { | project | assert_select "ul#projects>li.project>h2", :text => project.name }
+    end
+    
+    it 'renders the logo of the project' do
+      render
+      
+      @projects.each do | project |
+        rendered.should have_selector('img', :src => project.logo, :alt => project.name)
+      end
+    end
+    
+    it 'renders the description of the project' do
+      render
+      
+      @projects.each { | project | assert_select "ul#projects>li.project>.description", :text => project.description }
+    end
+    
+    it 'renders the show link' do
+      render
+      
+      @projects.each do | project |
+        rendered.should have_selector('a', :href => link_to(project), :text => t('projects.show_link_name'))
+      end
+    end
+    
+    it 'renders the edit link' do
+      render
+      
+      @projects.each do | project |
+        rendered.should have_selector('a', :href => edit_project_path(project), :text => t('projects.edit_link_name'))
+      end
+    end
+    
+    it 'renders the delete link' do
+      render
+      
+      @projects.each do | project |
+        rendered.should have_selector('a', :href => link_to(project, method => :delete), :text => t('projects.delete_link_name'))
+      end
+    end
   end
 end
--- test/factories/projects.rb
+++ test/factories/projects.rb
@@ -2,7 +2,7 @@
 FactoryGirl.define do
   factory :project do
       sequence :name do |n| "Project #{n}" end
-      description { "Description of #{name} " }
+      description { "Description of #{name}" }
       logo "/assets/icons/default_project.png"
       git_url "http://githost.foo.com/project"
       git_login "login"
