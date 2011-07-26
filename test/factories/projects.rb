4acbdaa57101d150e738e168aef44bf2cde5543d
David Rouchy <drouchy@gmail.com>
2011-07-26 16:36:05 +0200

    seeds for project with factories

--- test/factories/projects.rb
+++ test/factories/projects.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :project do
       sequence :name do |n| "Project #{n}" end
       description { "Description of #{name} " }
-      logo "/assets/icons/project.png"
+      logo "/assets/icons/default_project.png"
       git_url "http://githost.foo.com/project"
       git_login "login"
       git_password "password"
